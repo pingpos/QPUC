22df380a9400bbf47241296fcaa8dc5ed422c0b2
pingpos <oswald.arnaud@gmail.com>
2019-01-26 18:24:44 +0100

    V1

---
+++ data.js
@@ -0,0 +1,44 @@
+var questions = {
+	"manche1" : [		
+		{"question": "Est ce que ceci est un exemple ? ", "reponses": ["Oui", "Non", "Peut-être", "La réponse D"], "juste": 4 },
+		{"question": "Lequel de ces noms n'est pas dans la généalogie de Jésus ? (version Semeur)", "reponses": ["Yoda", "Salma", "Addi", "Athos"], "juste": 4 },
+		{"question": "Parmi les mots suivants, lequels ne figurent pas dans la liste du fruit de l'Esprit ?", "reponses": ["Courage", "Douceur", "Paix", "Bonté"], "juste": 1 },
+		{"question": "Quel est le principe que Martin Luther King prônait ?", "reponses": ["La contre-violance", "La non-violence", "L'absence de violence", "L'anti-violence"], "juste": 2 },
+		{"question": "Quel est le verset le plus long de la Bible ?", "reponses": ["Psaume 119.105", "Lévitique 12.5", "Esther 8.9", "Apocalypse 3.6"], "juste": 3 },
+		{"question": "Dans quel livre biblique parle-t-on pour la première fois de fromage ?", "reponses": ["Exode", "1 Rois", "1 Samuel", "Esther"], "juste": 3 },
+		{"question": "Par quel animal a été tué un prophète désobéissant ?", "reponses": ["Serpent", "Lion", "Scorpion", "Ours"], "juste": 2 },
+		{"question": "Quel roi prolongea sa vie de 15 ans grâce à une priére ?", "reponses": ["Josaphat", "David", "Josias", "Ezéchias"], "juste": 4 },
+		{"question": "Quel est le disciple qui a coupé une oreille au soldat venu arrêter Jésus ?", "reponses": ["Pierre", "Jean", "Jacques", "Matthieu"], "juste": 1 },
+		{"question": "Dans quel pays, la Bible fut-elle diffusée pour la première fois ?", "reponses": ["France", "Palestine", "Allemagne", "Egypte"], "juste": 3 },
+
+		{"question": "Dans la Bible à deux reprises des animaux prennent la parole. Lesquels sont-ils ?", "reponses": ["Le serpent (Genèse 3) / L'âne (Nombres 22 v21-34)"]},
+		{"question": "Qui fut vendu pour 20 pièces d'argent ?", "reponses": ["Joseph"]},		
+		{"question": "Quelle femme fut payée pour allaiter son bébé ?", "reponses": ["La mère de Moïse"] },
+		{"question": "Quel est le seul livre de la Bible où le nom de Dieu n'est pas mentioné ?", "reponses": ["Esther"] },
+		{"question": "Qui a prôné le régime végétarien ?", "reponses": ["Daniel"] },
+		{"question": "Comment s'appelle la femme qui a refusé de céder sa place dans un bus aux USA ?", "reponses": ["Rosa Parks"] }
+	]	
+	,
+	"manche2": [
+		{"question": "musiques/Yahwe.m4a", "reponses": ["Yahwé - Collectif Cieux Ouverts"], "time":50},
+		{"question": "musiques/SolaGratia.m4a", "reponses": ["Sola Gratia (Ta grâce seule) - Impact"], "time": 0},
+		{"question": "musiques/MON ANCRE ET MA VOILE.ogg", "reponses": ["Mon ancre et ma voile"], "time": 12},
+		{"question": "musiques/This Little Light.m4a", "reponses": ["This Little Light - LZ7"], "time": 0},
+		{"question": "musiques/Christ est la lumière.ogg", "reponses": ["Christ est la lumière - Matt Marvane"], "time": 0},
+		{"question": "musiques/Ca me donne des ailes.m4a", "reponses": ["Ca me donne des ailes - Antydot'"], "time": 0},
+		{"question": "musiques/Nous annoncons le Roi.m4a", "reponses": ["Nous annonçons le Roi"], "time": 0},
+		{"question": "musiques/Je chanterais Gloire à L'Eternel.ogg", "reponses": ["Je chanterais Gloire - Matt Marvane"], "time": 18},
+		{"question": "musiques/Nous dansons.m4a", "reponses": ["Nous dansons - Glorious"], "time": 0},
+		{"question": "musiques/Lumière du monde.m4a", "reponses": ["Lumière du monde"], "time": 44},
+		{"question": "musiques/COMME UN PHARE.m4a", "reponses": ["Comme un phare"], "time": 10},
+		{"question": "musiques/RegneEnMoi.m4a", "reponses": ["Règne En Moi"], "time": 20},
+		{"question": "musiques/INFINIMENT GRAND.ogg", "reponses": ["Infiniment grand - Impact"], "time": 0},
+		{"question": "musiques/Oceans.ogg", "reponses": ["Oceans - Hillsong"], "time": 0},
+		{"question": "musiques/God's Not Dead.m4a", "reponses": ["God's Not Dead (Like a lion) - Newsboys"], "time": 22},
+		{"question": "musiques/Abba Pere.ogg", "reponses": ["Abba Pere - Collectif Cieux Ouverts"], "time": 0},
+		{"question": "musiques/Laissons Entrer.m4a", "reponses": ["Laissons Entrer - Dan Luiten"], "time": 0},
+		{"question": "musiques/Vases d'Argile.m4a", "reponses": ["Vases d'Argile"], "time": 0},
+		{"question": "musiques/Never Once.m4a", "reponses": ["Never Once - Matt Redman"], "time":0},
+	]
+		
+};
